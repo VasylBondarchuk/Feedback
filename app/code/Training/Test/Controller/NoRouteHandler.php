11f0dec63320e1770007a7be478de83ec4f2abce
Vasyl Bondarchuk <teorfiz@gmail.com>
2021-09-27 00:30:44 +0300

    problem 2.3

---
+++ app/code/Training/Test/Controller/NoRouteHandler.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Training\Test\Controller;
+
+class NoRouteHandler implements \Magento\Framework\App\Router\NoRouteHandlerInterface
+{
+    public function process(\Magento\Framework\App\RequestInterface $request)
+    {
+        $moduleName = 'cms';
+        $controllerPath = 'index';
+        $controllerName = 'index';
+        $request->setModuleName($moduleName)
+            ->setControllerName($controllerPath)
+            ->setActionName($controllerName);
+        return true;
+    }
+}
