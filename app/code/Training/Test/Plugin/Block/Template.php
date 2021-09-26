a4a5e4dfa8650a00031ba5a45ff509c71e462554
Vasyl Bondarchuk <teorfiz@gmail.com>
2021-09-26 17:00:16 +0300

    problem 1.8.3

--- app/code/Training/Test/Plugin/Block/Template.php
+++ app/code/Training/Test/Plugin/Block/Template.php
@@ -8,8 +8,10 @@ class Template
         \Magento\Framework\View\Element\Template $subject,
         $result
     ) {
-        $result = '<div><p>' . $subject->getTemplate() . '</p>'
-            . '<p>' . get_class($subject) . '</p>' . $result . '</div>';
+        if ($subject->getNameInLayout() == 'top.search') {
+            $result = '<div><p>' . $subject->getTemplate() . '</p>'
+                . '<p>' . get_class($subject) . '</p>' . $result . '</div>';
+        }
         return $result;
     }
 }
