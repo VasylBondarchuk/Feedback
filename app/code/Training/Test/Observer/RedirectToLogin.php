d094dd2053d8de886a1ccb249fdb4e169271ce15
Vasyl Bondarchuk <teorfiz@gmail.com>
2021-10-01 20:45:18 +0300

    problem 1.10-edited

--- app/code/Training/Test/Observer/RedirectToLogin.php
+++ app/code/Training/Test/Observer/RedirectToLogin.php
@@ -18,21 +18,29 @@ class RedirectToLogin implements ObserverInterface
      * @param \Magento\Framework\App\Response\RedirectInterface $redirect
      * @param \Magento\Framework\App\ActionFlag $actionFlag
      */
+
+    private $customerSession;
+
     public function __construct(
         \Magento\Framework\App\Response\RedirectInterface $redirect,
-        \Magento\Framework\App\ActionFlag $actionFlag
+        \Magento\Framework\App\ActionFlag $actionFlag,
+        \Magento\Customer\Model\Session $customerSession
     ) {
         $this->redirect = $redirect;
         $this->actionFlag = $actionFlag;
+        $this->customerSession = $customerSession;
     }
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
         $request = $observer->getEvent()->getData('request');
-        if ($request->getModuleName() == 'catalog'
+
+        /*if ($request->getModuleName() == 'catalog'
             && $request->getControllerName() == 'product'
             && $request->getActionName() == 'view'
-        ) {
-// if ($request->getFullActionName() == 'catalog_product_view') { // altenative way
+        ) {*/
+
+        // redirect not logged user to login page if visits a product page
+        if ($request->getFullActionName() == 'catalog_product_view' && !$this->customerSession->isLoggedIn()) { // altenative way
             $controller = $observer->getEvent()->getData('controller_action');
             $this->actionFlag->set('', \Magento\Framework\App\ActionInterface::FLAG_NO_DISPATCH,true);
             $this->redirect->redirect($controller->getResponse(), 'customer/account/login');
