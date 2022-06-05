8b75c42fb3f9bc7a1badb5878180d58e701de645
Vasyl Bondarchuk <teorfiz@gmail.com>
2022-06-05 23:26:33 +0300

    Add top menu link name from configuration menu

--- Plugin/Topmenu.php
+++ Plugin/Topmenu.php
@@ -2,31 +2,75 @@
 
 namespace Training\Feedback\Plugin;
 
+use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\Data\Tree\NodeFactory;
 use Magento\Framework\UrlInterface;
+use Magento\Store\Model\ScopeInterface;
 
+/**
+ * Class generates top menu link
+ */
 class Topmenu
 {
-    protected $nodeFactory;
-    protected $urlBuilder;
+    private const TOP_MENU_LINK_NAME_PATH =
+        'feedback_configuration/feedback_configuration_general/top_menu_link_name';
 
-    public function __construct(NodeFactory $nodeFactory, UrlInterface $urlBuilder)
+    /**
+     * @var NodeFactory
+     */
+    protected NodeFactory $nodeFactory;
+
+    /**
+     * @var UrlInterface
+     */
+    protected UrlInterface $urlBuilder;
+    /**
+     * @var ScopeConfigInterface
+     */
+    protected ScopeConfigInterface $scopeConfig;
+
+    /**
+     * @param NodeFactory $nodeFactory
+     * @param UrlInterface $urlBuilder
+     * @param ScopeConfigInterface $scopeConfig
+     */
+    public function __construct(
+        NodeFactory $nodeFactory,
+        UrlInterface $urlBuilder,
+        ScopeConfigInterface $scopeConfig
+    )
     {
         $this->nodeFactory = $nodeFactory;
         $this->urlBuilder = $urlBuilder;
+        $this->scopeConfig = $scopeConfig;
     }
 
+    /**
+     * @param \Magento\Theme\Block\Html\Topmenu $subject
+     * @param $outermostClass
+     * @param $childrenWrapClass
+     * @param $limit
+     * @return void
+     */
     public function beforeGetHtml(\Magento\Theme\Block\Html\Topmenu $subject, $outermostClass = '', $childrenWrapClass = '', $limit = 0)
     {
-        $menuNode = $this->nodeFactory->create(['data' => $this->getNodeAsArray("Store Feedbacks", "index"),
+        $menuNode = $this->nodeFactory->create(
+            ['data' => $this->getNodeAsArray(
+            $this->getTopMenuLinkName(), "index"),
             'idField' => 'id',
-            'tree' => $subject->getMenu()->getTree()]);
+            'tree' => $subject->getMenu()->getTree()]
+        );
         /*$menuNode->addChild($this->nodeFactory->create(['data' => $this->getNodeAsArray("MainMenu", "mnuMyMenu"),
             'idField' => 'id',
             'tree' => $subject->getMenu()->getTree(),]));*/
         $subject->getMenu()->addChild($menuNode);
     }
 
+    /**
+     * @param $name
+     * @param $id
+     * @return array
+     */
     protected function getNodeAsArray($name, $id): array
     {
         $url = $this->urlBuilder->getUrl("training_feedback/index/" . $id); //here you can add url as per your choice of menu
@@ -35,5 +79,15 @@ class Topmenu
             'url' => $url,
             'has_active' => false,
             'is_active' => false,];
+    }
+
+    /**
+     * Gets name of the link in the top menu
+     *
+     * @return string
+     */
+    public function getTopMenuLinkName(): string
+    {
+        return $this->scopeConfig->getValue(self::TOP_MENU_LINK_NAME_PATH, ScopeInterface::SCOPE_STORE);
     }
 }
