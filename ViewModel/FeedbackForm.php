01489be0847c84c5d5fc7ee094d9facb12fffc54
VasylBondarchuk <teorfiz@gmail.com>
2023-02-07 00:39:48 +0200

    View model Minor refactoring

--- ViewModel/FeedbackForm.php
+++ ViewModel/FeedbackForm.php
@@ -7,9 +7,11 @@ use Magento\Customer\Api\CustomerRepositoryInterface;
 use Magento\Customer\Model\Session;
 use Magento\Framework\UrlInterface;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
+use Psr\Log\LoggerInterface;
+
 
 /**
- *
+ * View Model for a Feedback's front-end form
  */
 class FeedbackForm implements ArgumentInterface
 {
@@ -27,6 +29,11 @@ class FeedbackForm implements ArgumentInterface
      * @var CustomerRepositoryInterface
      */
     private CustomerRepositoryInterface $customerRepository;
+    
+    /**
+     * @var LoggerInterface
+     */
+    private LoggerInterface $logger;
 
     /**
      * @param UrlInterface $urlBuilder
@@ -36,37 +43,56 @@ class FeedbackForm implements ArgumentInterface
     public function __construct(
         UrlInterface $urlBuilder,
         Session $customerSession,
-        CustomerRepositoryInterface $customerRepository
+        CustomerRepositoryInterface $customerRepository,
+        LoggerInterface   $logger    
     ) {
         $this->urlBuilder = $urlBuilder;
         $this->customerSession = $customerSession;
         $this->customerRepository = $customerRepository;
+        $this->logger = $logger;
     }
 
-    /**
+     /**
+     * Gets customer's name
+     * 
      */
     public function getCustomerName() : string
     {
-        try {
-            $customerId =  (int)$this->customerSession->getCustomer()->getId();
-            $customerName = $this->customerRepository->getById($customerId)->getFirstname();
-        } catch (\Exception $exception) {
+        try {            
+            $customerName = $this->customerRepository
+                    ->getById($this->getCustomerId())
+                    ->getFirstname();
+        } catch (\Exception $e) {
             $customerName = '';
+            $this->logger->error($e->getLogMessage());
         }
         return $customerName;
     }
     
     /**
+     * Gets customer's email
+     * 
      */
     public function getCustomerEmail() : string
     {
-        try {
-            $customerId =  (int)$this->customerSession->getCustomer()->getId();
-            $customerEmail = $this->customerRepository->getById($customerId)->getEmail();
-        } catch (\Exception $exception) {
+        try {            
+            $customerEmail = $this->customerRepository
+                    ->getById($this->getCustomerId())
+                    ->getEmail();
+        } catch (\Exception $e) {
             $customerEmail = '';
+            $this->logger->error($e->getLogMessage());
         }
         return $customerEmail;
+    }
+    
+    /**
+     * Gets customer's Id
+     * 
+     */
+    public function getCustomerId() : int
+    {
+        return (int)$this->customerSession->getCustomer()->getId();       
     }
 
     /**
